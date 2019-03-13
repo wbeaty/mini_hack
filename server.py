84f533910a0804bfbe7c90c31177fdc7dd67d9ba
PJ-Leyden <pleyden@kent.edu>
2019-03-12 20:11:34 -0400

    Completed server

--- server.py
+++ server.py
@@ -1,23 +1,11 @@
-import socket
+#!/usr/bin/python
 
-HOST, PORT = '', 8888
+import SimpleHTTPServer
+import SocketServer
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
-print 'Serving HTTP on port %s ...' % PORT
-while True:
-    client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024)
-    path = request[4:find(request, " ")]
-    print path
-    print request
+PORT = 8000
 
-    http_response = """\
-HTTP/1.1 200 OK
+httpd = SocketServer.TCPServer(("", PORT), SimpleHTTPServer.SimpleHTTPRequestHandler)
 
-Hello, World!
-"""
-    client_connection.sendall(http_response)
-    client_connection.close()
+print "Serving at port", PORT
+httpd.serve_forever()
